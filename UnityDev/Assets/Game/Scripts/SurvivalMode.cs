0a5d069a45d7eb9f65ade3b760535aa023fb0571
Marcel van Duijn <epicmars264@gmail.com>
2018-06-20 14:33:56 +0200

    Commit

---
+++ UnityDev/Assets/Game/Scripts/SurvivalMode.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurvivalMode : MonoBehaviour
+{
+    ObjectPool_Script objectPoolScript;
+    [SerializeField]private GameObject m_Player;
+    [SerializeField]private GameObject[] m_ObjectPools;
+    [SerializeField]private GameObject[] m_SpawnPostitions;
+    [SerializeField]private float m_DifficultyIncrease;
+    [SerializeField]private int m_EnemyStartAmount;
+
+    private float m_Timer;
+    private Vector2 m_Difficulty;
+    private int m_NightSurvived;
+
+    void Start ()
+    {
+        PlayerController playerScript = m_Player.GetComponent<PlayerController>();
+	}
+	
+	void Update ()
+    {
+        m_Timer -= 1 * Time.deltaTime;
+        if(m_Timer <= 0)
+        {
+
+
+
+            //Reset Spawn Timer
+            m_Timer = Random.Range(m_Difficulty.x, m_Difficulty.y);
+        }
+	}
+
+    void SpawnEnemy(int enemyID)
+    {
+        int spawnPos = Random.Range(0, m_SpawnPostitions.Length);
+        for (int i = 0; i < objectPoolScript.objects.Count; i++)
+        {
+            if (!objectPoolScript.objects[i].activeInHierarchy)
+            {
+                objectPoolScript.objects[i].transform.position = m_SpawnPostitions[spawnPos].transform.position;
+                objectPoolScript.objects[i].transform.rotation = transform.rotation;
+                objectPoolScript.objects[i].SetActive(true);
+                break;
+            }
+        }
+    }
+}
---
+++ UnityDev/Assets/PostProcessing/Utilities/CustomMotionTexture/CameraSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.PostProcessing;
+using UnityEngine;
+
+public class CameraSettings : MonoBehaviour
+{
+    public PostProcessingProfile test;
+
+	void Update ()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            float dist = Vector3.Distance(hit.point, transform.position);
+            var amount = test.depthOfField.settings;
+            amount.focusDistance = dist;
+            test.depthOfField.settings = amount;
+        }
+    }
+}
+
+
+//[HelpURL("www.google.nl")]
+//[DisallowMultipleComponent]
+//    [TextArea]
