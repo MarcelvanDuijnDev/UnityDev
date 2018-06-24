aa2742ab4eca9faa8d3a2544ac1c4a733769b542
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2018-06-24 23:06:15 +0200

    Commit

---
+++ UnityDev/Assets/Game/Scripts/Weather.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weather : MonoBehaviour 
+{
+    [SerializeField]private Vector3 m_SunRotation;
+
+	void Start () 
+    {
+		
+	}
+	
+	void Update () 
+    {
+		
+	}
+}
