bb910fc29e43f7443ba55e67c256faedc667964a
Marcel van Duijn <epicmars264@gmail.com>
2018-06-25 13:05:02 +0200

    Commit

--- UnityDev/Assets/Game/Scripts/Weather.cs
+++ UnityDev/Assets/Game/Scripts/Weather.cs
@@ -2,17 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weather : MonoBehaviour 
+public class Weather : MonoBehaviour
 {
-    [SerializeField]private Vector3 m_SunRotation;
+    [SerializeField]private GameObject sunObj;
+    [SerializeField]private Vector3 m_Time;
+    [SerializeField]private int m_DaysInYear;
+    [SerializeField]private int m_CurrentYear, m_CurrentDay;
 
-	void Start () 
+    private float m_TotalTime, speed, m_TimeCalculate;
+    private Light m_SunLight;
+
+    void Start()
     {
-		
-	}
-	
-	void Update () 
+        m_SunLight = sunObj.GetComponent<Light>();
+
+        m_TotalTime += m_Time.x * 60 * 60;
+        m_TotalTime += m_Time.y * 60;
+        m_TotalTime += m_Time.z;
+        m_TimeCalculate = m_TotalTime;
+        speed = 360 / m_TotalTime;
+        m_CurrentDay = 1;
+    }
+
+    void Update()
     {
-		
-	}
+        sunObj.transform.Rotate(Vector3.right * speed * Time.deltaTime);
+
+        //Calculate Day/Year
+        m_TimeCalculate -= 1 * Time.deltaTime;
+        if (m_TimeCalculate <= 0)
+        {
+            m_CurrentDay += 1;
+            m_TimeCalculate = m_TotalTime;
+        }
+        if (m_CurrentDay >= m_CurrentYear)
+        {
+            m_CurrentYear += 1;
+            m_CurrentDay = 1;
+        }
+
+        //Weather state
+        
+        //rain emmision rate
+    }
 }
